71d1d3bb4226ed702098385abc0987c0849c797c
S <s>
2015-02-03 19:55:57 +0200

    {"type":"TI", "runs":"1"}

--- exerciseCh4.java
+++ exerciseCh4.java
@@ -5,7 +5,8 @@ public class exerciseCh4 extends ConsoleProgram {
 	public void run () {
 		exerciseOne();
 	}
-	
+
+	// We output the song 99 beers on the wall
 	private void exerciseOne () {
 		for (int i = 99 ; i > 0; i--) {
 			println (i + " bottles of beer on the wall.");
@@ -16,4 +17,16 @@ public class exerciseCh4 extends ConsoleProgram {
 		}
 		println ("And we are done.");
 	}
+	
+	// We change the word on the second line
+	private void exerciseTwo () {
+		String word;
+		for (int i = 1 ; i <= 10 ; i++) {
+			if (i=1) word = "thumb";
+			println (i);
+				
+		}
+	}
+	
+
 }
