6a5397dcdcba9b277960259463058f4bbc5f9ea3
S <s>
2015-02-03 19:45:49 +0200

    {"type":"TI", "runs":"1"}

--- exerciseCh4.java
+++ exerciseCh4.java
@@ -3,6 +3,10 @@ import acm.program.ConsoleProgram;
  
 public class exerciseCh4 extends ConsoleProgram {
 	public void run () {
+		exerciseOne();
+	}
+	
+	private void exerciseOne () {
 		for (int i = 99 ; i > 0; i--) {
 			println (i + " bottles of beer on the wall.");
 			println (i + " bottles of beer.");
