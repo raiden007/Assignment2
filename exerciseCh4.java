44b0c529dab3c3dd99096eb0a21e92fc6a4b905a
S <s>
2015-02-03 19:44:22 +0200

    {"type":"TI", "runs":"1"}

--- exerciseCh4.java
+++ exerciseCh4.java
@@ -5,9 +5,10 @@ public class exerciseCh4 extends ConsoleProgram {
 	public void run () {
 		for (int i = 10 ; i > 0; i--) {
 			println (i + " bottles of beer on the wall.");
-			println (i + " bottles of beer");
+			println (i + " bottles of beer.");
 			println ("You take one down, pass it around.");
 			println ((i-1) + " bottles of beer on the wall.");
+			println ();
 		}
 		println ("And we are done.");
 	}
