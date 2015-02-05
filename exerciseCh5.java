7ae611891424ed2bd6633e2239e1b18bb18e3080
S <s>
2015-02-05 19:57:27 +0200

    {"type":"TI", "runs":"0"}

--- exerciseCh5.java
+++ exerciseCh5.java
@@ -1,4 +1,13 @@
-
-public class exerciseCh5 {
-
+import acm.program.ConsoleProgram;
+ 
+public class exerciseCh5 extends ConsoleProgram {
+	public void run () {
+		exerciseOne();
+	}
+	
+	
+	private void exerciseOne () {
+		double constant = (1+Math.sqrt(5))/2;
+		println (constant);
+	}
 }
