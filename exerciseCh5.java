882de1183fdd0d1d1022a42dcaa98d15e9c61533
S <s>
2015-02-05 21:01:58 +0200

    {"type":"TI", "runs":"1"}

--- exerciseCh5.java
+++ exerciseCh5.java
@@ -86,7 +86,9 @@ public class exerciseCh5 extends ConsoleProgram {
 			for (int i=0;i>k;i--) {
 				double fraction = 1;
 				fraction = (fraction*x);
+				println(fraction);
 				result = 1 / fraction;
+				println(result);
 			}
 			
 		} else {
