407f0463f4f230fd491ab6800f52e67ec079ce28
S <s>
2015-02-05 20:49:42 +0200

    {"type":"TI", "runs":"1"}

--- exerciseCh5.java
+++ exerciseCh5.java
@@ -83,7 +83,7 @@ public class exerciseCh5 extends ConsoleProgram {
 	private double raiseRealToPower (double x, int k) {
 		double result=1;
 		if (k<0) {
-			for (int i=0;i<k;i++) {
+			for (int i=0;i>k;i--) {
 				result= 1/(result*x);			
 			}
 		} else {
