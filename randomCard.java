49df8ddb092e5bf22ff60db4eae0a9626cf5426b
S <s>
2015-02-09 14:39:48 +0200

    {"type":"TI", "runs":"0"}

--- randomCard.java
+++ randomCard.java
@@ -1,12 +1,23 @@
-import acm.program.ConsoleProgram;
+import acm.program.*;
+import acm.util.*;
 
 
 public class randomCard extends ConsoleProgram {
 	public void run () {
-		exerciseOne();
+		cardNumber();
 	}
 	
-	public void exerciseOne() {
-		
+	private int randomNumber() {
+		int card = rgen.nextInt (1,13);
+		return card;
 	}
+	
+	private int cardNumber() {
+		int number = randomNumber();
+		println(number);
+		return number;
+	}
+	
+	private RandomGenerator rgen = new RandomGenerator();
+	
 }
