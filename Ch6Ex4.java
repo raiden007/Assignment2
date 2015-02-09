da8f8f4a3429b82c219ee57cb75ab3a0e9207976
S <s>
2015-02-09 16:28:57 +0200

    {"type":"TI", "runs":"0"}

--- Ch6Ex4.java
+++ Ch6Ex4.java
@@ -1,7 +1,29 @@
 import acm.program.*;
+import acm.util.RandomGenerator;
 
 public class Ch6Ex4 extends ConsoleProgram {
 	public void run() {
-		
+		int initialAtoms = 10000;
+		int years = 0;
+		int atoms = initialAtoms;
+		for (int i=0;i<atoms;i++) {
+			String atomsDecayed = atomDecayed();
+			if (atomsDecayed=="Decayed") {
+				atoms--;
+				println(atoms);
+			} else {
+				println(atoms);
+			}
+		}
 	}
+	
+	private RandomGenerator rgen = new RandomGenerator();
+	
+	private String atomDecayed() {
+		String decayed = rgen.nextBoolean() ? "Decayed" : "NotDecayed";
+		return decayed;
+	}
+	
+
+	
 }
