c283dd14c47ba33d5eaf0d9e476451e3a9c83338
S <s>
2015-02-09 14:52:22 +0200

    {"type":"TI", "runs":"3"}

--- randomCard.java
+++ randomCard.java
@@ -6,7 +6,7 @@ public class randomCard extends ConsoleProgram {
 	public void run () {
 		int cardNumber = cardNumber();
 		if (cardNumber==1) {
-			println ("Your card is Ace");
+			println ("Your card is Ace ");
 		} else if (cardNumber==11){
 			println ("Your card is Jack");
 		} else if (cardNumber==12) {
@@ -16,18 +16,36 @@ public class randomCard extends ConsoleProgram {
 		} else {
 			println("Your card is "+cardNumber);
 		}
+		int cardSuite = randomSuite();
+		if (cardSuite==1) {
+			print("of Clubs.");
+		} else if (cardSuite==2) {
+			print("of Diamonds.");
+		} else if (cardSuite==3) {
+			print("of Hearts.");
+		} else {
+			print("of Spades.");
+		}
+		
 	}
 	
-	private int randomNumber() {
+	private int randomCardNumber() {
 		int card = rgen.nextInt (1,13);
 		return card;
 	}
 	
 	private int cardNumber() {
-		int number = randomNumber();
+		int number = randomCardNumber();
 		println(number);
 		return number;
 	}
+	
+	private int randomSuite() {
+		int suite = rgen.nextInt(1,4);
+		println(suite);
+		return suite;
+	}
+	
 	
 	private RandomGenerator rgen = new RandomGenerator();
 	
