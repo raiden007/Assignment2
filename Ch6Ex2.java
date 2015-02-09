9092cd5ece1e9419aff1c9bd6b5b3008a61ff802
S <s>
2015-02-09 20:41:15 +0200

    {"type":"TI", "runs":"0"}

--- Ch6Ex2.java
+++ Ch6Ex2.java
@@ -1,3 +1,7 @@
+/* This program flips a coin until you get 3 heads
+ * results in a row
+ */
+
 import acm.program.*;
 import acm.util.*;
 
@@ -8,12 +12,15 @@ public class Ch6Ex2 extends ConsoleProgram {
 		int heads=0;
 		int tails=0;
 		int nrOfFlips=0;
+		// Until there are 3 heads we flip a coin
 		while (heads<3) {
 			String flip = flipCoin();
 	//		println("FlipCoin: "+flip);
 			nrOfFlips++;
+			// Increments heads count if coin is heads
 			if (flip=="Heads") {
 				heads++;
+			// Increments tails count(debug) and resets the heads count
 			} else if (flip=="Tails"){
 				tails++;
 				heads=0;
@@ -22,15 +29,18 @@ public class Ch6Ex2 extends ConsoleProgram {
 	//		println("Tails :"+tails);	
 			
 		}
+		// Prints the final result
 		println ("It took "+nrOfFlips+" flips to get "+heads+" consecutive heads.");
 		
 	}
 	
+	// Flips a coin and gets a result
 	private String flipCoin() {
 		String coinFlip = rgen.nextBoolean() ? "Heads" : "Tails";
 		return coinFlip;
 	}
 	
+	// Random generator constructor
 	private RandomGenerator rgen = new RandomGenerator();
 	
 }
