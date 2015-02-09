d82df0871caf9ec3b0824fbb5903072bc8d5bc21
S <s>
2015-02-09 20:38:43 +0200

    {"type":"TI", "runs":"0"}

--- Ch6Ex1.java
+++ Ch6Ex1.java
@@ -8,7 +8,9 @@ import acm.util.*;
 
 public class Ch6Ex1 extends ConsoleProgram {
 	public void run () {
+		// Gets the card number
 		int cardNumber = cardNumber();
+		// Prints the correct text
 		if (cardNumber==1) {
 			print ("Your card is Ace ");
 		} else if (cardNumber==11){
@@ -20,7 +22,9 @@ public class Ch6Ex1 extends ConsoleProgram {
 		} else {
 			print("Your card is "+cardNumber+" ");
 		}
+		// Gets the suite color
 		int cardSuite = randomSuite();
+		// Prints the next part of the text
 		if (cardSuite==1) {
 			print("of Clubs.");
 		} else if (cardSuite==2) {
@@ -33,24 +37,27 @@ public class Ch6Ex1 extends ConsoleProgram {
 		
 	}
 	
+	// Gets a random number as the card number
 	private int randomCardNumber() {
 		int card = rgen.nextInt (1,13);
 		return card;
 	}
 	
+	// Gets a random number between 1-13 to determine the card
 	private int cardNumber() {
 		int number = randomCardNumber();
 	//	println(number);
 		return number;
 	}
 	
+	// Gets a random nr between 1-4 to determine the suite
 	private int randomSuite() {
 		int suite = rgen.nextInt(1,4);
 	//	println(suite);
 		return suite;
 	}
 	
-	
+	// Constructor for the random generator
 	private RandomGenerator rgen = new RandomGenerator();
 	
 }
