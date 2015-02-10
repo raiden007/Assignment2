73cc4c9bdc98184b74dc8348413693af3d81697e
S <s>
2015-02-10 14:38:20 +0200

    {"type":"TI", "runs":"1"}

--- Ch6Ex5.java
+++ Ch6Ex5.java
@@ -9,7 +9,7 @@ public class Ch6Ex5 extends ConsoleProgram {
 	
 	public void run() {
 		// Seed 5 tests that the result is less than zero
-		rgen.setSeed(1);
+		rgen.setSeed(2);
 		exerciseFive();
 	}
 	
