d4f18898326819cefad0cec912c1574405fd3242
S <s>
2015-02-10 14:32:55 +0200

    {"type":"TI", "runs":"0"}

--- Ch6Ex5.java
+++ Ch6Ex5.java
@@ -1,4 +1,47 @@
+import acm.program.*;
+import acm.util.RandomGenerator;
 
-public class Ch6Ex5 {
+public class Ch6Ex5 extends ConsoleProgram {
+	
+	int appropiateRange = 20;
+	
+	public void run() {
+		rgen.setSeed(1);
+		exerciseFive();
+	}
+	
+	private void exerciseFive() {
+		int nrOfQuestions = 5;
+		String operation = operationRandom();
+		int firstNumber=numberRandom();
+		int secondNumber=numberRandom();
+		int result;
 
+		if (operation=="+") {
+			result = firstNumber+secondNumber;
+		} else {
+			result = firstNumber-secondNumber;
+		}
+		
+		println(firstNumber);
+		println(secondNumber);
+		println(result);
+		
+	}
+	
+	private RandomGenerator rgen = new RandomGenerator();
+	
+	private String operationRandom() {
+		String operation = rgen.nextBoolean() ? "+" : "-";
+		return operation;
+		
+	}
+	
+	private int numberRandom () {
+		int number = rgen.nextInt(1,appropiateRange);
+		return number;
+	}
+	
+	
+	
 }
