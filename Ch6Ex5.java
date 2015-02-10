258560f3ff7eef95c9b939d2da640360717947d6
S <s>
2015-02-10 14:38:00 +0200

    {"type":"TI", "runs":"1"}

--- Ch6Ex5.java
+++ Ch6Ex5.java
@@ -3,10 +3,13 @@ import acm.util.RandomGenerator;
 
 public class Ch6Ex5 extends ConsoleProgram {
 	
-	int appropiateRange = 20;
+	int minRange = 0;
+	int maxRange = 20;
+
 	
 	public void run() {
-		rgen.setSeed(5);
+		// Seed 5 tests that the result is less than zero
+		rgen.setSeed(1);
 		exerciseFive();
 	}
 	
@@ -17,10 +20,15 @@ public class Ch6Ex5 extends ConsoleProgram {
 		int secondNumber=numberRandom();
 		int result;
 		
-		while (firstNumber-secondNumber<0) {
+		while (firstNumber-secondNumber<minRange) {
 			firstNumber=numberRandom();
 			secondNumber=numberRandom();
 		}
+		
+//		while (firstNumber+secondNumber>maxRange) {
+	//		firstNumber=numberRandom();
+//			secondNumber=numberRandom();
+//		}
 
 		if (operation=="+") {
 			result = firstNumber+secondNumber;
