8fa2ba1e0dff9a5fd9f155aa5aedc5162aac0829
S <s>
2015-02-10 14:54:01 +0200

    {"type":"TI", "runs":"0"}

--- Ch6Ex5.java
+++ Ch6Ex5.java
@@ -45,7 +45,7 @@ public class Ch6Ex5 extends ConsoleProgram {
 		println ("Welcome to Math Quiz!");
 		for (int i=0; i<nrOfQuestions; i++) {
 			int userResult = readInt("What is "+firstNumber+" "+operation+" "+secondNumber+" ? ");
-			int userResult = readInt("");
+	//		int userResult = readInt("");
 			if (userResult==result) {
 				println("Correct!");
 			} else {
