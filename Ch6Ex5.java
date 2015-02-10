0b5477f4dd91b02708d45f931fdacab606a30763
S <s>
2015-02-10 14:59:50 +0200

    {"type":"TI", "runs":"1"}

--- Ch6Ex5.java
+++ Ch6Ex5.java
@@ -47,6 +47,8 @@ public class Ch6Ex5 extends ConsoleProgram {
 			int userResult = readInt("What is "+firstNumber+" "+operation+" "+secondNumber+" ? ");
 			if (userResult==result) {
 				println("Correct!");
+				firstNumber=numberRandom();
+				secondNumber=numberRandom();
 			} else {
 				int tries=0;
 					while (tries<3) {
