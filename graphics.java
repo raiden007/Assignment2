fedf5189323e8f481a5d94224fac7566b7480578
S <s>
2015-02-12 15:49:17 +0200

    {"type":"TI", "runs":"1"}

--- graphics.java
+++ graphics.java
@@ -1,14 +1,15 @@
 import java.awt.Color;
-
+import acm.util.*;
 import acm.graphics.*;
-import acm.program.ConsoleProgram;
+import acm.program.*;
 import acm.program.GraphicsProgram;
 
 
 public class graphics extends GraphicsProgram {
 	public void run () {
-		GObject rect = new GRect(100,100,100,100);
+		GRect rect = new GRect(100,100,100,100);
 		rect.setColor(Color.RED);
+		rect.setFilled(true);
 		add (rect);
 	}
 }
