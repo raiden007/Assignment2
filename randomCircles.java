996444b8742e472b5d674491555d4514f0e88c87
S <s>
2015-02-12 16:36:15 +0200

    {"type":"TI", "runs":"4"}

--- randomCircles.java
+++ randomCircles.java
@@ -7,13 +7,12 @@ import acm.program.*;
 public class randomCircles extends GraphicsProgram {
 	public void run () {
 //		for (int i=0;i<10;i++) {
-//			circle.setColor(rgen.nextColor());
+			circle.setColor(rgen.nextColor());
 			int radious = setRandomRadius();
 			int xPosition = setRandomHeightPosition();
 			int yPosition = setRandomWidthPosition();
 //			println(getWidth());
 //			println(getHeight());
-
 			GOval circle = new GOval (xPosition,yPosition,radious,radious);
 			println(xPosition);
 			println(yPosition);
