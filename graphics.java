786ef5c2dfd9093014a23cc6d901780a2fb8b34a
S <s>
2015-02-12 15:50:26 +0200

    {"type":"TI", "runs":"1"}

--- graphics.java
+++ graphics.java
@@ -11,5 +11,12 @@ public class graphics extends GraphicsProgram {
 		rect.setColor(Color.RED);
 		rect.setFilled(true);
 		add (rect);
+
+		for (int i=0;i<100;i++) {
+			pause(1000);
+			rect.setVisible(false);
+			pause(1000);
+			rect.setVisible(true);
+		}
 	}
 }
