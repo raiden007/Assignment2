d48815645e42b80df1e07fda2bc67c5df2f1a878
S <s>
2015-02-12 15:50:53 +0200

    {"type":"TI", "runs":"1"}

--- graphics.java
+++ graphics.java
@@ -7,15 +7,16 @@ import acm.program.GraphicsProgram;
 
 public class graphics extends GraphicsProgram {
 	public void run () {
+		int pauseTime = 500;
 		GRect rect = new GRect(100,100,100,100);
 		rect.setColor(Color.RED);
 		rect.setFilled(true);
 		add (rect);
 
 		for (int i=0;i<100;i++) {
-			pause(1000);
+			pause(pauseTime);
 			rect.setVisible(false);
-			pause(1000);
+			pause(pauseTime);
 			rect.setVisible(true);
 		}
 	}
