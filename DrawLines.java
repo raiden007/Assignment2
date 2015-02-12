7156f20041c56190c3727c1b7d08a030ad4b8f74
S <s>
2015-02-12 16:56:14 +0200

    {"type":"TI", "runs":"1"}

--- DrawLines.java
+++ DrawLines.java
@@ -34,7 +34,7 @@ public class DrawLines extends GraphicsProgram {
       double y = Math.min(e.getY(), startY);
       double width = Math.abs(e.getX() - startX);
       double height = Math.abs(e.getY() - startY);
-      currentLine.setEndPoint(width,height);
+      currentLine.setEndPoint(height,width);
 }
    
 /* Private state */
