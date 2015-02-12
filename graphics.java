d16222158d181d068623e0f4b33309a50d3dbd7c
S <s>
2015-02-12 15:42:40 +0200

    {"type":"TI", "runs":"0"}

--- graphics.java
+++ graphics.java
@@ -1,4 +1,12 @@
+import acm.graphics.*;
+import acm.graphics.GRect;
+import acm.program.ConsoleProgram;
+import acm.program.GraphicsProgram;
 
-public class graphics {
 
+public class graphics extends GraphicsProgram {
+	public void run () {
+		GObject rect = new GRect(0,0,100,100);
+		add (rect);
+	}
 }
